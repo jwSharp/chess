07e63da59654c70559200fcf40dae2196f5a0a8c
amycuff317 <amycuff317@gmail.com>
2022-07-11 17:02:45 -0400

    Created Title Screen Template file Child of State Class

---
+++ Title_Screen.py
@@ -0,0 +1,15 @@
+# This will be the Title Screen/ Menu file for Chess
+# This is a CHILD of the State Class
+# Template file to be edited later
+# Will use some form of time/ timer
+
+
+class Title(State):
+	def __init__(self, game):
+		State.__init__(self, game)
+
+	def update(self, time, actions):
+		pass
+
+	def render(self, display):
+		pass
