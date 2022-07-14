b10d6b0cce268fe9bb9a879777efdb8de2a1f378
Jacob Sharp <sharkmann.j@gmail.com>
2022-07-14 20:02:36 +0000

    Simplify piece.py class so that it follows OOP. Still needs some work.

--- piece.py
+++ piece.py
@@ -1,108 +1,56 @@
-import pygame
+import board.py
+import player.py
 
+##################
+# Abstract Class #
+##################
 class Piece:
-    def __init__(self, sprite: pygame.image, start_pos: (int, int), piece_name = 'pawn', turn: int = 0, piece_moves = [('0', '1'), ('F', '0', '1|2')], different_attacks = None):
-      self.sprite = sprite
-      self.start_pos = start_pos
-      self.pos = start_pos
-      self.current_pos = start_pos
-      self.piece_name = piece_name
-      self.turn = turn
-      self.piece_moves = piece_moves
-      self.different_attacks = different_attacks
-      self.move_count = 0
-      self.capturables = []
-      
+    '''Represents a game piece.'''
     
-    def move_piece(self, x, y, other_pieces):
-        if self.movable(x, y, other_pieces):
-          self.current_pos = (x, y)
-          self.pos = (x, y)
-          self.move_count += 1
-        else:
-          self.pos = self.current_pos
-        return self.current_pos
-    
-    def force_move(self, x, y, condition = True):
-        if condition:
-          self.current_pos = (x, y)
-          self.move_count += 1
-        return self.current_pos
-    
-    def movable(self, target_x, target_y, pieces) -> bool:
-        if target_x < 0 or target_x > 7 or target_y < 0 or target_y > 7:
-            return False
-        return (target_x, target_y) in self.get_movement(pieces) or (target_x, target_y) in self.get_capturables(pieces)
-    
-    def get_movement(self, pieces):
-        blocks = []
-        self.capturables = []
-        piece_positions = [p.current_pos for p in pieces]
-  
-        for i in [list(move) for move in self.piece_moves]:
-          if 'F' in i and self.move_count != 0 or self.current_pos == None:
-            continue
-          pos = [(self.current_pos[0] + j[0], self.current_pos[1] - j[1]) for j in self.movable_blocks(i)]
-          # if pieces != []:
-          for p in pos:
-            if p not in [piece.current_pos for piece in pieces]:
-              blocks.append(p)
-            else:
-              if self.different_attacks == None and pieces[piece_positions.index(p)].turn != self.turn:
-                self.capturables.append(p)
-              break
-          else:
-            blocks += pos
-        return blocks
+    @staticmethod #TODO make a static method
+    class Location:
+        '''x and y coordinates on the board.'''
+        def __init__(x_coord: int, y_coord: int):
+            self.x, self.y = x_coord, y_coord
+        def __init__(coordinates: (int, int)):
+            self.x, self.y = coordinates
 
-    def get_capturables(self, pieces):
-        piece_positions = [p.current_pos for p in pieces]
-        if self.different_attacks == None:
-            self.get_movement(pieces)
+    @staticmethod #TODO make a static variable
+    _ways_to_move = []
+
+
+    @staticmethod #TODO make a static variable
+    PATH = 'Resources/Game_Pieces/' #TODO set based on path
+    @staticmethod #TODO make a static variable
+    _sprites = [] #TODO set based on path
+
+    def __init__(self, position: (int, int), team: int, board: Board):
+        self.board = board
+        self.sprite = _set_sprite(team)
+        self.team = team
+
+        self.board_position = Location(position)
+        self.real_time_position = Location(position)
+
+
+    def move(self, destination: (int, int)):
+        if not is_legal_move(self, Location(destination)):
+            #TODO illegal move
+            self.real_time_position = self.board_position
         else:
-            self.capturables = []
-            attack_list = [list(attack) for attack in self.different_attacks]
-            for i in attack_list:
-                if 'F' in i and self.move_count != 0 or self.current_pos == None:
-                    continue
-                pos = [(self.current_pos[0] + j[0], self.current_pos[1] - j[1]) for j in self.movable_blocks(i)]
-                for p in pos:
-                  if p in [piece.current_pos for piece in pieces]:
-                    if pieces[piece_positions.index(p)].turn != self.turn:
-                      self.capturables.append(p)
-                    break
-        return self.capturables
-  
-    def movable_blocks(self, area):
-        blocks = []
-        pos_x = []
-        pos_y = []
-        if '|' in area[-2]:
-          while area[-2].split('|')[0] != area[-2].split('|')[1]:
-            pos_x.append(int(area[-2].split('|')[0]))
-            if int(area[-2].split('|')[0]) < int(area[-2].split('|')[1]):
-              area[-2] = str(int(area[-2].split('|')[0]) + 1) + '|' + area[-2].split('|')[1]
-            else:
-              area[-2] = str(int(area[-2].split('|')[0]) - 1) + '|' + area[-2].split('|')[1]
-        else:
-          pos_x = [int(area[-2])]
-        
-        if '|' in area[-1]:
-          while area[-1].split('|')[0] != area[-1].split('|')[1]:
-            pos_y.append(int(area[-1].split('|')[0]))
-            if int(area[-1].split('|')[0]) < int(area[-1].split('|')[1]):
-              area[-1] = str(int(area[-1].split('|')[0]) + 1) + '|' + area[-1].split('|')[1]
-            else:
-              area[-1] = str(int(area[-1].split('|')[0]) - 1) + '|' + area[-1].split('|')[1]
-        else:
-          pos_y = [int(area[-1])]
-        
-        for i in range(max([len(pos_x), len(pos_y)])):
-            x = pos_x[-1] if pos_x.index(pos_x[-1]) < i else pos_x[i]
-            y = pos_y[-1] if pos_y.index(pos_y[-1]) < i else pos_y[i]
-            blocks.append((x, y))
-        return blocks
-      
-    def destroy_piece(self):
-        self.current_pos = None
-        self.pos = None
+            #TODO if capture
+            #TODO 
+    
+    def remove(self):
+        '''Removes piece from the board.'''
+        #TODO move to graveyard instead
+        self.board_position = None
+        self.real_time_position = None
+
+        #TODO updates to team stats
+
+
+    def _set_sprite(team: int) -> str:
+        '''Returns a string containing the path to the correct png.'''
+        _sprites = 'Resources/Game_Pieces/'
+        return '' #TODO return [images in _sprites folder][team]
