dbc191073825454e7588d195807b9711bc14fc91
Jacob Sharp <sharkmann.j@gmail.com>
2022-07-26 17:07:14 +0000

    Move piece.py updates to pieces.py. Make Piece an abstract class.

--- main.py
+++ main.py
@@ -8,21 +8,22 @@ from gui import *
 def main():
     pygame.init()
     pygame.display.set_caption("Chess")
+    
     while True:
-        CLOCK.tick(60)
-        # drawing board, letters numbers arround the board, and pieces on the board
         SCREEN.fill(BLACK)
         make_chess_GUI()
         chess.draw_letters(GOLD, 36, "elephant")
         chess.draw_board()
         chess.draw_pieces()
-        # event handling
+
         for event in pygame.event.get():
             chess.handle_events(event)
             if event.type == pygame.QUIT:
                 pygame.quit()
                 quit()
+        
         pygame.display.update()
 
+        CLOCK.tick(60)
 
 main()
