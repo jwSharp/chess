417c97c074a54995700fd498a7b0385453cb59cf
mehmet0zen <mehmetozen159@gmail.com>
2022-08-01 18:23:56 -0400

    Move Timer class to accessories.

--- accessories.py
+++ accessories.py
@@ -1,3 +1,6 @@
+import pygame
+from config import *
+
 class Button:
     def __init__(self, image, position: (int, int), name_text: str, font_type, base_color, hover_color):
         self.image = image
@@ -34,5 +37,49 @@ class Button:
             self.text = self.font.render(self.name, True, self.base_color)
 
 class Timer:
-    def __init__(self, time: (int, int)):
-        pass
+    def __init__(self, minute, additional, manager):
+        self.minute = minute
+        self.additional_seconds = additional
+        self.second = 0
+        self.manager = manager
+        self.format_time()
+
+    def time_mechanics(self):
+        self.minute = int(self.minute)
+        self.second = int(self.second)
+        if self.minute > 0:
+            if self.second > 0:
+                self.second -= 1
+                return (self.minute, self.second)
+            self.minute -= 1
+            self.second = 59
+            return (self.minute, self.second)
+        if self.second > 0:
+            self.second -= 1
+            return (self.minute, self.second)
+        return (0, 0)
+
+    def format_time(self):
+        if (self.minute, self.second) == (0, 0):
+            self.timer = "Time's Up!"
+            return
+        if self.minute < 10:
+            self.minute = str(0) + str(self.minute)
+        if self.second < 10:
+            self.second = str(0) + str(self.second)
+        self.timer = f"{self.minute}:{self.second}"
+
+    def update(self):
+        self.time_mechanics()
+        self.format_time()
+
+    def add_additional(self):
+        pass #TODO: Add additional seconds
+
+    def draw(self, pos, size, screen):
+        font = GET_FONT("Timer", size)
+        timer = font.render(self.timer, True, (255, 255, 255))
+        timer_rect = timer.get_rect()
+        timer_rect.center = pos
+        screen.blit(timer, timer_rect)
+        
