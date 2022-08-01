202029443cc878059f23fb06a37534a59ef9f9bc
Jacob Sharp <sharkmann.j@gmail.com>
2022-08-01 21:01:18 +0000

    Add another player slot.

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def main():
     pygame.display.set_caption("Main Menu")
 
     # Create Player
-    players = [Human('Player 1'), None, None]
+    players = [Human('Player 1'), None, None, None]
 
     # Create Scene Manager
     manager = SceneManager(players)
